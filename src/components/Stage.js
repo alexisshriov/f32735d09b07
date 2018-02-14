5096c79f83f6113509e3d037f484692797eccfe4
alexisshriov <alexis.st21@gmail.com>
2018-02-14 06:54:52 -0500

    asdasd
    asd

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -30,19 +30,24 @@ class Stage extends React.Component {
     }
 
     render() {
+        let backgroundColor = this.state.stageHeigth.interpolate({
+            inputRange: [0, 20000, 40000, 60000],
+            outputRange: ['rgb(0,150,255)','rgb(0,84,199)','rgb(0,64,152)', 'rgb(0,18,43)']
+        });
+
         return (
 
-            <View style={{ height: this.windowHeigth, backgroundColor: "#7EC0EE" }}>
+            <Animated.View style={{ height: this.windowHeigth, backgroundColor: backgroundColor }}>
                 <Text style={{ fontSize: 50, zIndex:2, color: "red", alignSelf: 'center', marginTop: 10 }}>{(this.state.displayedHeight)}</Text>
                 <Animated.View style={{ width: this.windowWidth, height: this.windowHeigth, marginTop: this.state.stageHeigth }}>
                     <ImageBackground source={require("../../images/stage.png")} style={{  flex: 1, position: 'absolute', width: this.windowWidth, height: this.windowHeigth-200, justifyContent: 'flex-end', alignItems: 'center' }}>
                         <Square stageWidth={this.windowWidth} stageHeigth={this.windowHeigth} initialLaunch={this.initialLaunch} updateHeigthLabel = {this.updateHeigthLabel} moveStage = {this.moveStage} />
                     </ImageBackground>
                 </Animated.View>
-                <Animated.View style={{top: -1000 + this.state.displayedHeight/16, position:'absolute', alignSelf:'center', height:50, width:50, backgroundColor: 'red'}}></Animated.View>
+                <Animated.View style={{top: -1000 + this.state.displayedHeight/32, position:'absolute', alignSelf:'center', height:50, width:50, backgroundColor: 'red'}}></Animated.View>
                 <Image source={require("../../images/sun3.png")} style={{top: -50 + this.state.displayedHeight/16, position:'absolute', marginLeft: 50, height:400, width:400}}/>
-                <Animated.View style={{top: -550 + this.state.displayedHeight/16, position:'absolute', height:50, width:50, backgroundColor: 'white'}}></Animated.View>
-            </View>
+                <Animated.View style={{top: -550 + this.state.displayedHeight/32, position:'absolute', height:50, width:50, backgroundColor: 'white'}}></Animated.View>
+            </Animated.View>
 
         )
     }
